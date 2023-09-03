4bb2b20ef7776ecb6d40943f864b5e9953200b82
Mehboob Ali Bhat <mehboobali.bhat@gmail.com>
2023-09-03 22:23:09 +0530

    Added Tailwind and Poscss Nesting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="min-h-screen flex flex-col justify-center items-center">
+	<img
+		class="w-80 mb-4 rounded-xl"
+		src="https://fakeimg.pl/320x260/EEE0C9/"
+		alt="Under Construction"
+	/>
+	<h1 class="text-5xl m-6 font-semibold text-gray-800">Site Under Construction</h1>
+	<p class="text-gray-600">We are working hard to bring you something awesome.</p>
+	<p class="text-gray-600 font-bold">Come back soon!</p>
+</div>
